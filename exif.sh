34d8d7ac6fe340690ff129adde8109d22b5fdf29
raza-jafy <31965255+raza-jafy@users.noreply.github.com>
2024-03-31 20:49:13 +1300

    Update exif.sh

--- exif.sh
+++ exif.sh
@@ -5,7 +5,7 @@
 curl -s "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/domains.txt" | anew target-wildcards.txt
 
 # Run gau to collect subdomains and save them to urls.txt
-cat target-wildcards.txt | gau --subs --threads 16 | anew gau-urls.txt | uro | anews unique-urls.txt
+cat target-wildcards.txt | gau --subs --threads 16 | anew gau-urls.txt | uro | anew unique-urls.txt
 
 cat unique-urls.txt | grep -E '\.(docx|rtf|jpeg|png|svg|img|tsv|csv|odt)$' | httpx -silent -mc 200 | anew urls.txt 
 rm unique-urls.txt
@@ -23,11 +23,11 @@ process_document() {
     
     # Search for specific text in the document
     if grep -q "internal use only|confidential" temp.txt; then
-        echo "Found sensitive text in: $url && notify -silent -id Confidential-Exif | tee document-finding.txt"
+        echo "Found sensitive text in: $url && notify -silent -id Confidential-Exif1, Confidential-Exif2 | tee document-finding.txt"
     fi
     
     # Run exiftool on the document
-    exiftool "$file" | grep -E "Latitude|Longitude|GPSLocation|comments|passwords|password|URL" && echo "\nFound metadata in: $url && notify -silent -id Confidential-Exif | tee exif-finding.txt"
+    exiftool "$file" | grep -E "Latitude|Longitude|GPSLocation|comments|passwords|password|URL" && echo "\nFound metadata in: $url && notify -silent -id Confidential-Exif1, Confidential-Exif2| tee exif-finding.txt"
     
     # Clean up temporary files
     rm temp.txt
@@ -52,7 +52,7 @@ for ext in "${image_extensions[@]}"; do
         curl -s "$url" -o temp.$ext
         
         # Run exiftool on the image
-        exiftool "temp.$ext" | grep -E "Latitude|GPSLocation|Longitude|comments|passwords|password|URL" && echo "\nFound metadata in: $url" && echo "\nMetadata: $metadata && notify -silent -id Confidential-Exif | tee exif-finding.txt"
+        exiftool "temp.$ext" | grep -E "Latitude|GPSLocation|Longitude|comments|passwords|password|URL" && echo "\nFound metadata in: $url" && echo "\nMetadata: $metadata && notify -silent -id Confidential-Exif1, Confidential-Exif2  | tee exif-finding.txt"
         
         # Clean up temporary files
         rm "temp.$ext"
