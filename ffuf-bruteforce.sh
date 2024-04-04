a128bcfbd4a43cf9606af95f7b8b95696174a4f7
raza-jafy <31965255+raza-jafy@users.noreply.github.com>
2024-04-05 06:20:32 +1300

    Update ffuf-bruteforce.sh

--- ffuf-bruteforce.sh
+++ ffuf-bruteforce.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-# Array to hold the wordlist filenames
-wordlists=("onelistforallaa" "onelistforallab" "onelistforallac" "onelistforallad" "onelistforallae" "onelistforallaf" "onelistforallag" "onelistforallah" "onelistforallai" "onelistforallaj")
-
-# Function to run gobuster on domains.txt with each wordlist
-run_gobuster() {
-    local domain_file="$1"
-    for wordlist in "${wordlists[@]}"; do
-        echo "Running FFUF on $domain_file with wordlist: $wordlist.txt"
-        while IFS= read -r domain; do
-            echo "Domain: $domain" | notify -silent -pc provider-config.yaml
-            ffuf -w "$wordlist" -u "$domain_fileFUZZ" -ac -mc 200,301,403 -t 20 -se -o "$domain_file" | notify -silent -pc provider-config.yaml
-        done < "$domain_file"
-    done
-}
-
-# Call the function with the domains.txt file
-run_gobuster "domains.txt"
+ffuf -w onelistforallaa -u https://dreambox.my.salesforce.com/FUZZ -mc 200 -ac -fl 47 | notify -silent -pc provider-config.yaml
