f4257bd9b33b203d177c786db3c6d49fe8fbc2af
raza-jafy <31965255+raza-jafy@users.noreply.github.com>
2024-04-07 07:59:29 +1200

    Update and rename nucleri-fuzzing.sh to nuclei-fuzzing.sh

---
+++ nuclei-fuzzing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -f urls.txt
+rm -f live-urls.txt
+excluded_extentions="png,jpg,gif,jpeg,swf,woff,svg,pdf,json,css,js,webp,woff,woff2,eot,ttf,otf,mp4,txt"
+# ANSI color codes
+green='\033[0;32m'
+reset='\033[0m'
+
+
+echo -e "${green}Running Paramspider${reset}"
+/root/ParamSpider/paramspider -l nowaf.txt --exclude "$excluded_extentions" --level high --quiet | anew urls.txt
+
+echo -e "${green}Running Httpx${reset}"
+cat urls.txt | httpx -silent -mc 200,301,302 | uro | anew live-urls.txt
+
+echo -e "${green}Running Nuclei${reset}"
+nuclei -l live-urls.txt -t "/root/fuzzing-templates" -fr -iserver uxxbakcpqehrnrfowwuxguhai0imvotcr.oast.fun -fuzz -debug-req -rl 05 | notiffy
