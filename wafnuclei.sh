3ebf6bd175bf633e6153fb4f35bc63254d138a8e
raza-jafy <31965255+raza-jafy@users.noreply.github.com>
2024-04-07 02:20:11 +1300

    Update wafnuclei.sh

--- wafnuclei.sh
+++ wafnuclei.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rm -f domains.txt
+rm -f results.txt
+
+# ANSI color codes
+green='\033[0;32m'
+reset='\033[0m'
+
+# Echo "Running Subfinder" in green
+echo -e "${green}Running Subfinder${reset}"
 subfinder -d $domain -all -silent -o domains.txt
 
 # Define input and output files
@@ -7,17 +16,18 @@ input_file="domains.txt"
 output_file="nowaf.txt"
 
 # Remove output file if it already exists
-rm -f "$input_file"
 rm -f "$output_file"
 
+echo -e "${green}Running WafW00f${reset}"
 # Loop through each domain in the input file
 while IFS= read -r domain; do
     # Run wafw00f on the domain and check if WAF detected
-    if wafw00f "$domain" | grep -qi "No WAF detected"; then
+    if wafw00f "$domain" | grep -qi "No WAF detected by the generic detection"; then
         # If no WAF detected, save the domain to the output file
         echo "$domain" >> "$output_file"
     fi
 done < "$input_file"
 
+echo -e "${green}Running Nuclei${reset}"
 # Run nuclei on domains without WAF
-nuclei -l nowaf.txt -s critical,high,medium,low -fr -iserver uxxbakcpqehrnrfowwuxguhai0imvotcr.oast.fun -o results.txt | notify -silent -pc provider-config.yaml 
+nuclei -l nowaf.txt -s critical,high,medium,low -fr -iserver uxxbakcpqehrnrfowwuxguhai0imvotcr.oast.fun -o results.txt | notify -silent -pc provider-config.yaml
