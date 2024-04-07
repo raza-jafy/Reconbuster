e4cc4ff07040dfba1af0fbd5449310750d394b15
raza-jafy <31965255+raza-jafy@users.noreply.github.com>
2024-04-07 20:40:21 +1200

    Update ffuf-bruteforce.sh

--- ffuf-bruteforce.sh
+++ ffuf-bruteforce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffuf -w onelistforallaa -u https://dreambox.my.salesforce.com/FUZZ -mc 200 -ac -fl 47 | notify -silent -pc provider-config.yaml
+ffuf -w onelistforallaa -u https:///FUZZ -mc 200 -ac -fl 47 | notify -silent -pc provider-config.yaml
